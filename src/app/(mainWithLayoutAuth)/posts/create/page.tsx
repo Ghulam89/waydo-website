98d2f32001c72e13e55696ba5cbb5ba854200895
Ghulam89 <gm6681328@gmail.com>
2025-08-09 08:19:34 -0700

    update

--- src/app/(mainWithLayoutAuth)/posts/create/page.tsx
+++ src/app/(mainWithLayoutAuth)/posts/create/page.tsx
@@ -3,15 +3,26 @@
 import { useAppDispatch, useAppSelector } from "@redux/hooks";
 import { clearCreatePostForm } from "@redux/slices/post";
 import { useEffect } from "react";
-
-
-import LastFormPost from "@components/pages/post/form/steps/last-form";
-
-import FirstFormPost from "@components/pages/post/form/steps/first-form";
-import SelectTypePost from "@components/pages/post/form/steps/select-type-post";
+import dynamic from 'next/dynamic';
 import config from "@config";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import style from "./page.module.css";
+
+// Dynamically import components with SSR disabled
+const SelectTypePost = dynamic(
+  () => import('@components/pages/post/form/steps/select-type-post'),
+  { ssr: false }
+);
+
+const FirstFormPost = dynamic(
+  () => import('@components/pages/post/form/steps/first-form'),
+  { ssr: false }
+);
+
+const LastFormPost = dynamic(
+  () => import('@components/pages/post/form/steps/last-form'),
+  { ssr: false }
+);
 
 export default function CreatePost() {
   const dispatch = useAppDispatch()
@@ -31,17 +42,9 @@ export default function CreatePost() {
       <div className={style.content}>
         <div className={style.box}>
           <div>
-            {step === 0 && (
-              <SelectTypePost show />
-            )}
-
-            {step === 1 && (
-              <FirstFormPost show />
-            )}
-
-            {step === 2 && (
-              <LastFormPost show />
-            )}
+            {step === 0 && <SelectTypePost show />}
+            {step === 1 && <FirstFormPost show />}
+            {step === 2 && <LastFormPost show />}
           </div>
         </div>
       </div>
